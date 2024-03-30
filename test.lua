b4c852caee5f88725ec0fb11bb77745d07651307
BALARA <1fewl1@mail.ru>
2024-03-30 18:40:31 +0300

    Залил проект

--- test.lua
+++ test.lua
@@ -1,11 +1,50 @@
+local com=require("component")
 
-robot=require'robot'
+local arg={...}
+local glass=com.glasses
+glass.removeAll()
+local geo=com.geolyzer
+
+local size=16
+local pl=3
+local minpl = 2
+local maxpl = 5
+
+local maxY = 1
+local function tocolor(pl)
+  local color = (pl-minpl)/maxpl
+  if color<0 then
+    return {1,1,1}
+  elseif color>1 then
+    return {1,0,1}
+  else
+    return {color,1-color,0}
+  end
+end
+
+local function create(x,y,z,p)
+    local widget = glass.addCube3D()
+    widget.addTranslation(x + 0.125, y + 0.125, z + 0.125)
+    widget.addScale(0.75, 0.75, 0.75)
+    widget.setVisibleThroughObjects(true)
+
+    local color = tocolor(p)
+    color[4] = 1
+
+    widget.addColor(table.unpack(color))
+  end
+end
+
+for x=-size,size do
+  for z=-size,size do
+    tile=geo.scan(x,z)
+    os.sleep(0)
+    for Y=-math.min(size,18),math.min(maxY,18) do
+      local y=Y+32
+      if tile[y]>pl then create(x,Y,z,tile[y]) end
+    end
+  end
+end
 
 
-robot.forward()
-robot.back()
-robot.up()
-robot.down()
-robot.turnLeft()
-robot.turnRight()
-robot.turnAround()
+print("found objects:", (glass.getObjectCount or glass.getWidgetCount)())
